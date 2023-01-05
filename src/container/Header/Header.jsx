261795c82feba9c83f11ce5b3ab34c35498be554
Ellisvelandia <eyis619l@gmail.com>
2023-01-05 15:30:39 -0500

    navbar & header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Present the Menu and Drink List - As the guests are seated, present each
+        diner with the menu and wine list. Do not hand off the menus in a rush.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
